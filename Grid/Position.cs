b119446c1567168f2399acab11098488eb8d9266
Jia1213733590 <1213733590@qq.com>
2026-01-02 11:07:17 +0000

    Initial commit: implement Position system

---
+++ Grid/Position.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TurnBasedStrategy.Grid
+{
+    public class Position
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Position Move(int dx, int dy)
+        {
+            return new Position(X + dx, Y + dy);
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using TurnBasedStrategy.Grid;
+
+var position = new Position(2, 3);
+var newPosition = position.Move(1, -1);
+
+Console.WriteLine($"Original: {position}");
+Console.WriteLine($"Moved: {newPosition}");
+
